f584500a80bd5b39a9da2d49763e27f0a3f7777c
EvGanOff <evgan66@gmail.com>
2021-06-18 13:03:27 +0300

    dz3

---
+++ DZ3/DZ3/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DZ3
+//
+//  Created by AWAIN on 18.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+    }
+
+
+}
+
