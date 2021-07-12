d6f06493a206ca69817f5f2c5497d909b38b0549
EvGanOff <evgan66@gmail.com>
2021-07-12 13:29:45 +0300

    DZ 9 fix

--- DZ 9/DZ 9/ViewController.swift
+++ DZ 9/DZ 9/ViewController.swift
@@ -16,46 +16,34 @@ protocol ViewControllerResultDelegate: class {
 class ViewController: UIViewController, ViewControllerResultDelegate {
     
     func result(_ textField1: Int, textField2: String?) {
-        let color1 = UIColor(red: 150 / 255, green: 80 / 255, blue: 50 / 255, alpha: 1).cgColor
-        let color2 = UIColor(red: 210 / 255, green: 20 / 255, blue: 61 / 255, alpha: 1).cgColor
+        self.view.layer.sublayers?.first(where: {$0 is CAGradientLayer})?.removeFromSuperlayer()
+        
+     
+        let color3 = UIColor(red: 150 / 255, green: 80 / 255, blue: 50 / 255, alpha: 1).cgColor
+        let color4 = UIColor(red: 210 / 255, green: 20 / 255, blue: 61 / 255, alpha: 1).cgColor
         let gradietLayer1 = CAGradientLayer()
         gradietLayer1.frame = view.bounds
-        gradietLayer1.colors = [color1, color2]
+        gradietLayer1.colors = [color3, color4]
         gradietLayer1.startPoint = CGPoint(x: 0, y: 0)
         gradietLayer1.endPoint = CGPoint(x: 0, y: 1.0)
-        let color3 = UIColor(red: 150 / 255, green: 150 / 255, blue: 50 / 255, alpha: 1).cgColor
-        let color4 = UIColor(red: 210 / 255, green: 150 / 255, blue: 61 / 255, alpha: 1).cgColor
+        let color5 = UIColor(red: 150 / 255, green: 150 / 255, blue: 50 / 255, alpha: 1).cgColor
+        let color6 = UIColor(red: 210 / 255, green: 150 / 255, blue: 61 / 255, alpha: 1).cgColor
         let gradietLayer2 = CAGradientLayer()
         gradietLayer2.frame = view.bounds
-        gradietLayer2.colors = [color3, color4]
+        gradietLayer2.colors = [color5, color6]
         gradietLayer2.startPoint = CGPoint(x: 0, y: 0)
         gradietLayer2.endPoint = CGPoint(x: 0, y: 1.0)
         if (textField1 > 50) && textField2 == "M" {
-            self.view.layer.insertSublayer(gradietLayer1, at: 1)
+            self.view.layer.insertSublayer(gradietLayer1, at: 0)
         }
         if (textField1 < 50) && textField2 == "M" {
             return
         }
         if  textField2 == "F" {
-            self.view.layer.insertSublayer(gradietLayer2, at: 2)
+            self.view.layer.insertSublayer(gradietLayer2, at: 0)
         }
         
     }
-
-//            switch textField1 {
-//            case  0..<50:
-//                return
-//            case 50..<999999:
-//                self.view.layer.insertSublayer(gradietLayer1, at: 1)
-//            default:
-//                break
-//            }
-//            switch textField2{
-//            case "F":
-//                self.view.layer.insertSublayer(gradietLayer2, at: 2)
-//            default:
-//                break
-//            }
         
   
     @IBOutlet weak var userName: UITextField!
