6d7a1f5973d43af26384f4476c791f2f6ac879e3
EvGanOff <evgan66@gmail.com>
2021-07-19 09:06:23 +0300

    DZ 11

---
+++ lesson 11/lesson 11/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  lesson 11
+//
+//  Created by AWAIN on 15.07.2021.
+//
+
+import UIKit
+var view1: UIView!
+var label: UILabel!
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var buttonAC: UIView!
+    @IBOutlet weak var buttonPlusMinus: UIView!
+    @IBOutlet weak var buttonProc: UIView!
+    @IBOutlet weak var buttonRelation: UIView!
+    @IBOutlet weak var buttonSeven: UIView!
+    @IBOutlet weak var buttonEight: UIView!
+    @IBOutlet weak var buttonNine: UIView!
+    @IBOutlet weak var buttonMultiply: UIView!
+    @IBOutlet weak var buttonFour: UIView!
+    @IBOutlet weak var buttonFive: UIView!
+    @IBOutlet weak var buttonSix: UIView!
+    @IBOutlet weak var buttonMinus: UIView!
+    @IBOutlet weak var buttonOne: UIView!
+    @IBOutlet weak var buttonTwo: UIView!
+    @IBOutlet weak var buttonThree: UIView!
+    @IBOutlet weak var buttonPlus: UIView!
+    @IBOutlet weak var buttonPoint: UIView!
+    @IBOutlet weak var buttonComp: UIView!
+    @IBOutlet weak var buttonZero: UIView!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        buttonAC.layer.cornerRadius = buttonAC.frame.size.width / 2
+        buttonPlusMinus.layer.cornerRadius = buttonPlusMinus.frame.size.width / 2
+        buttonProc.layer.cornerRadius = buttonProc.frame.size.width / 2
+        buttonRelation.layer.cornerRadius = buttonRelation.frame.size.width / 2
+        buttonSeven.layer.cornerRadius = buttonSeven.frame.size.width / 2
+        buttonEight.layer.cornerRadius = buttonEight.frame.size.width / 2
+        buttonNine.layer.cornerRadius = buttonNine.frame.size.width / 2
+        buttonMultiply.layer.cornerRadius = buttonMultiply.frame.size.width / 2
+        buttonFour.layer.cornerRadius = buttonFour.frame.size.width / 2
+        buttonFive.layer.cornerRadius = buttonFive.frame.size.width / 2
+        buttonSix.layer.cornerRadius = buttonSix.frame.size.width / 2
+        buttonMinus.layer.cornerRadius = buttonMinus.frame.size.width / 2
+        buttonOne.layer.cornerRadius = buttonOne.frame.size.width / 2
+        buttonTwo.layer.cornerRadius = buttonTwo.frame.size.width / 2
+        buttonThree.layer.cornerRadius = buttonThree.frame.size.width / 2
+        buttonPlus.layer.cornerRadius = buttonPlus.frame.size.width / 2
+        buttonPoint.layer.cornerRadius = buttonPoint.frame.size.width / 2
+        buttonComp.layer.cornerRadius = buttonComp.frame.size.width / 2
+        buttonZero.layer.cornerRadius = buttonZero.frame.size.width / 5
+        }
+}
+ 
+    
+
+  
+
+
